28dc520978965b8865156138016199bfd474cbea
kraused53 <kraused53@gmail.com>
2022-03-23 11:45:01 -0400

    Created get_weather_json() function.
    Inputs:
    > lat - Requestsed latitude
    > lon - Requested Longitude
    > exclusions - String list of data types to exclude from API response
    
    If inputs not given ->
        Use lat & lon for newyork and request all data
    
    Returns JSON packet for current weather data

---
+++ WeatherManager/weatherbot.py
@@ -0,0 +1,34 @@
+from OPEN_WEATHER_KEYS import OPEN_WEATHER_API_KEY
+from requests import get, exceptions
+from datetime import datetime
+
+
+# ----------------------------------------------------------------------------
+"""
+    Use the requests library to make an API call to Open Weather. If the 
+        request is successful, return the requestd data as a JSON data set.
+        If the request fails, return None data type. The None response is to
+        be handled by the caller of the function
+"""
+def get_weather_json(lat, lon, exclusions = ''):
+
+    API_URL = 'https://api.openweathermap.org/data/2.5/onecall?' +\
+        'lat=' + lat + '&lon=' + lon +\
+        '&exclude=' + exclusions +\
+        '&units=imperial' +\
+        '&appid=' + OPEN_WEATHER_API_KEY
+
+#    print(API_URL)
+
+    try:
+        response = get(API_URL)
+    except exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)
+    
+    # Check to make sure response.get() worked
+    if response is not None:
+        # Check for valid response
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
