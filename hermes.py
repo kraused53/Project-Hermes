bb5b9f466a3d6be8f42f68fb9f5eb0d3592ba744
kraused53 <kraused53@gmail.com>
2022-03-23 18:41:33 -0400

    created the main hermes program

--- hermes.py
+++ hermes.py
@@ -1,1 +1,11 @@
+# Import Hermes submodules
 from .EmailManager.emailmanager import send_text_email
+from .WeatherManager.weatherbot import get_weather_json
+from .WeatherManager.weatherbot import convert_time
+
+
+#
+
+
+if __name__ == '__main__':
+    pass
