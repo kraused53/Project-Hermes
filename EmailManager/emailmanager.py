53e66410bced4ff26c4413b4755b3a52ed7eafee
kraused53 <kraused53@gmail.com>
2022-03-23 18:08:11 -0400

    Started emailmanager.py subsystem

---
+++ EmailManager/emailmanager.py
@@ -0,0 +1,6 @@
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from re import T
+import smtplib, ssl
+import EMAIL_KEYS
