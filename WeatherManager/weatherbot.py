32e589659f2acb62e5a16a62dd69964248987d61
kraused53 <kraused53@gmail.com>
2022-03-23 17:46:56 -0400

    Added test sequence to weatherbot.py
    Runs only if weatherbot.py is run directly
    
    Gets weather data for NYC and prints data to terminal

--- WeatherManager/weatherbot.py
+++ WeatherManager/weatherbot.py
@@ -63,3 +63,61 @@ def get_weather_json(lat = '40.7128', lon = '-74.0030', exclusions = ''):
             return response.json()
         else:
             return None
+
+
+# ----------------------------------------------------------------------------
+"""
+    When 'weather-bot.py' is run as a program, this is where the program 
+        starts. If another python file is currently the active project, 
+        this section is ignored.
+
+    This is where I will test the weather-bot before it is added to the main 
+        Hermes Project.
+"""
+if __name__ == '__main__':
+    weather_json = get_weather_json()
+
+    if weather_json is not None:
+        if 'current' in weather_json:
+            print('Current Weather Forecast:')
+            # Format available items
+            # Format time data
+            if 'dt' in weather_json['current']:
+                print('\tCurrent Time:\t'+convert_time(weather_json['current']['dt'], weather_json['timezone_offset'], True)[11:])
+            if 'sunrise' in weather_json['current']:
+                print('\tSunrise:\t'+convert_time(weather_json['current']['sunrise'], weather_json['timezone_offset'], True)[11:])
+            if 'sunset' in weather_json['current']:
+                print('\tSunset:\t\t'+convert_time(weather_json['current']['sunset'], weather_json['timezone_offset'], True)[11:])
+            
+            # Add line between time and temp data
+            print(' ')
+
+            # Format temperature data
+            if 'temp' in weather_json['current']:
+                print('\tCurrent Temp:\t'+str(weather_json['current']['temp'])+' F')
+            if 'feels_like' in weather_json['current']:
+                print('\tFeels Like:\t'+str(weather_json['current']['feels_like'])+' F')
+            if 'dew_point' in weather_json['current']:
+                print('\tDew Point:\t'+str(weather_json['current']['dew_point'])+' F')
+            if 'pressure' in weather_json['current']:
+                print('\tPressure:\t'+str(weather_json['current']['pressure'])+' hPa')
+
+            # Add line between temp and sky data
+            print(' ')
+
+            # Format Sky Data
+            if 'uvi' in weather_json['current']:
+                print('\tUV Index:\t'+str(weather_json['current']['uvi'])+' ')
+            if 'clouds' in weather_json['current']:
+                print('\tCloud Cover:\t'+str(weather_json['current']['clouds'])+' %')
+            if 'humidity' in weather_json['current']:
+                print('\tHumidity:\t'+str(weather_json['current']['humidity'])+' %')
+            if 'visibility' in weather_json['current']:
+                print('\tVisibility:\t'+str(weather_json['current']['visibility'])+' meters')
+
+            if 'weather' in weather_json['current']:
+                if 'icon' in weather_json['current']['weather'][0]:
+                        icon_url = 'http://openweathermap.org/img/wn/' + \
+                            weather_json['current']['weather'][0]['icon'] + \
+                            '@2x.png'
+                        print(icon_url)
